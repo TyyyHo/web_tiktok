0cf6872c3f41fc77243b20c42e2f1a9bf668b53a
TyyyHo <ty.ho1126@gmail.com>
2024-03-15 16:35:38 +0800

    update: next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "matsubiennial-cms.s3.ap-northeast-3.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  // output: "export",
 };
 
 module.exports = nextConfig;
